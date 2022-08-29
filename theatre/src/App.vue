91b7bb5f850e1cc847e28ceb73db827ae25dab4e
RESHMASURESH20 <112317386+RESHMASURESH20@users.noreply.github.com>
2022-08-29 13:49:35 +0530

    Add files via upload

---
+++ theatre/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+   
+   
+    <HeaderComponent/>
+  
+    <MainComponent/>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script>
+
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+import MainComponent from './components/MainComponent.vue'
+export default {
+  name: 'App',
+  components: {
+   
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+      /* width: fit-content; */
+  margin-top: 0px;
+}
+@media screen and (max-width:1000px) {
+   #app {
+    width: fit-content;
+   }
+}
+</style>
---
+++ theatre/src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+        <router-view></router-view>
+        <!-- <AddMovieComponent/>
+        <AboutComponent />
+        <DescriptionComponent/>
+        <BookingComponent/>
+        <BookedDetailsComponent/> -->
+        </div>
+</template>
+<script src="../js/MainComponent.js">
+
+</script>
+<style >
+
+</style>
